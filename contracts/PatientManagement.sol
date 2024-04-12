250de468fbdf6711879bc40f7f242776fe2484fa
Tazrian08 <tazrian.hossain08@gmail.com>
2024-04-12 19:14:59 +0600

    first commit

---
+++ contracts/PatientManagement.sol
@@ -0,0 +1,56 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.7.0 <0.9.0;
+
+contract PatientManagement {
+
+   event votedEvent(uint indexed _candidateId);
+   event PatientAdded(
+       uint indexed id,
+       string name,
+       uint age,
+       string gender,
+       string vaccine_status,
+       string district,
+       string symptoms_details,
+       bool is_dead
+   );
+
+   // model a candidate
+   struct Patient{
+       uint id;
+       string name;
+       uint age;
+       string gender;
+       string vaccine_status;
+       string district;
+       string symptoms_details;
+       bool is_dead;
+   }
+
+   // Store accounts that have voted
+   mapping( address => bool ) public admins;
+
+   mapping( uint => Patient ) public patients;
+
+   // store candidates count
+   uint public patientsCount=0;
+
+   // adding candidates
+   function addPatient(string memory _name, uint _age ,string memory _gender,string memory _vaccine_status,string memory _district,string memory _symptoms_details,bool _is_dead) public {
+       patientsCount++;
+       patients[patientsCount] = Patient(patientsCount, _name,_age, _gender, _vaccine_status, _district, _symptoms_details, _is_dead);
+       emit PatientAdded(patientsCount, _name, _age, _gender, _vaccine_status, _district, _symptoms_details, _is_dead);
+   }
+
+   // cast vote
+   function makeAdmin(uint _candidateId) public {
+       // require that the current voter haven't voted before
+       require(!admins[msg.sender]);
+       /// emit the event
+       emit votedEvent(_candidateId);
+
+       // record voters vote
+       admins[msg.sender] = true;
+   }
+}
+
---
+++ src/js/app.js
@@ -0,0 +1,224 @@
+App = {
+ webProvider: null,
+ contracts: {},
+ account: '0x0',
+
+
+ init: function() {
+   return App.initWeb();
+ },
+
+
+ initWeb:function() {
+   // if an ethereum provider instance is already provided by metamask
+   const provider = window.ethereum
+   if( provider ){
+     // currently window.web3.currentProvider is deprecated for known security issues.
+     // Therefore it is recommended to use window.ethereum instance instead
+     App.webProvider = provider;
+   }
+   else{
+     $("#loader-msg").html('No metamask ethereum provider found')
+     console.log('No Ethereum provider')
+     // specify default instance if no web3 instance provided
+     App.webProvider = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'));
+   }
+
+
+   return App.initContract();
+ },
+
+
+initContract: function() {
+
+
+   $.getJSON("PatientManagement.json", function( PatientManagement ){
+     // instantiate a new truffle contract from the artifict
+     App.contracts.PatientManagement = TruffleContract( PatientManagement );
+
+
+     // connect provider to interact with contract
+     App.contracts.PatientManagement.setProvider( App.webProvider );
+
+
+     App.listenForEvents();
+
+
+     return App.render();
+   })
+
+
+ },
+
+
+
+render: async function(){
+   let PatientManagementInstance;
+   const loader = $("#loader");
+   const content = $("#content");
+
+
+   loader.show();
+   content.hide();
+  
+   // load account data
+   if (window.ethereum) {
+     try {
+       // recommended approach to requesting user to connect mmetamask instead of directly getting the accounts
+       const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+       App.account = accounts;
+       $("#accountAddress").html("Your Account: " + App.account);
+     } catch (error) {
+       if (error.code === 4001) {
+         // User rejected request
+         console.warn('user rejected')
+       }
+       $("#accountAddress").html("Your Account: Not Connected");
+       console.error(error);
+     }
+   }
+
+
+   //load contract ddata
+   App.contracts.PatientManagement.deployed()
+   .then( function( instance ){
+     PatientManagementInstance = instance;
+
+
+     return PatientManagementInstance.patientsCount();
+   }) 
+   .then( function( patientsCount ){
+     var patientsResults = $("#patientsResults");
+     patientsResults.empty();
+
+
+     var patientsSelect = $("#patientsSelect");
+     patientsSelect.empty();
+
+
+     for (let i = 1; i <= patientsCount; i++) {
+       PatientManagementInstance.patients( i )
+       .then( function( patient ){
+         var id = patient[0];
+         var gender = patient[1];
+         var vaccine_status= patient[2];
+        
+         // render results
+         var patientTemplate = "<tr><th>" + id + "</th><td>" + gender + "</td><td>" + vaccine_status+ "</td></tr>"
+         patientsResults.append( patientTemplate );
+
+
+         //render balloot option
+         let patientOption = "<option value=" + id +  ">" + name + "</option>"
+         patientsSelect.append( patientOption )
+       });
+     }
+     return PatientManagementInstance.admins(  App.account )
+   })
+   .then( function( isAdmin ){
+     // don't allow user to vote
+     if(isAdmin){
+       $( "form" ).hide()
+     }
+     loader.hide();
+     content.show();
+   })
+   .catch( function( error ){
+     console.warn( error )
+   });
+ },
+
+
+ // casting vote
+ castVote: function(){
+   App.contracts.PatientManagement.deployed()
+   .then( function( instance ){
+     return instance.makeAdmin(1,{ from: App.account[0] } )
+   })
+   .then( function( result ){
+     // wait for voters to update vote
+     console.log({ result })
+       // content.hide();
+       // loader.show();
+       alert("You are an admin")
+       App.render()
+   })
+   .catch( function( err ){
+     console.error( err )
+   } )
+ },
+ 
+ addPatient: function() {
+ let name= $("#nameInput").val();
+  let age = $("#ageInput").val();
+  let gender = $("#genderInput").val();
+  let vaccineStatus = $("#vaccineStatusInput").val();
+  let district = $("#districtInput").val();
+  let symptoms = $("#symptomsInput").val();
+  let isDead = $("#isDeadInput").val() === "true";
+
+  App.contracts.PatientManagement.deployed()
+  .then(function(instance) {
+    return instance.addPatient(name, age, gender, vaccineStatus, district, symptoms, isDead, { from: App.account[0] });
+  })
+  .then(function(result) {
+    // Success, do something
+    console.log("Patient added successfully:", result);
+    App.render()
+    // Optionally, you can update the UI to reflect the addition of the patient
+  })
+  .catch(function(error) {
+    // Handle errors
+    console.error("Error adding patient:", error);
+  });
+  
+},
+
+ // voted event
+ listenForEvents: function(){
+   App.contracts.PatientManagement.deployed()
+   .then( function( instance ){
+     instance.votedEvent({}, {
+       fromBlock: 0,
+       toBlock: "latests"
+     })
+     .watch( function( err, event ){
+       console.log("Triggered", event);
+       // reload page
+       App.render()
+     })
+   })
+ },
+ 
+listenForEvents: function() {
+   App.contracts.PatientManagement.deployed()
+   .then(function(instance) {
+       // Get the contract instance
+       const contractInstance = instance;
+
+       // Subscribe to the PatientAdded event
+       contractInstance.PatientAdded({}, function(error, event) {
+           if (error) {
+               console.error("Error listening for PatientAdded event:", error);
+               return;
+           }
+           console.log("Patient Added:", event.returnValues);
+           // Optionally, you can update the UI to reflect the addition of the patient
+           // For example, you can append the new patient details to a table
+           // or display a notification to the user
+       });
+   })
+   .catch(function(error) {
+       console.error("Error getting contract instance:", error);
+   });
+},
+
+};
+
+
+$(function() {
+ $(window).load(function() {
+   App.init();
+ });
+});
+
